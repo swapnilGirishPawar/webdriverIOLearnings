7824f78433ac4d68217ce15002a5fc35f368de0a
swapnil.pawar@gullak.money <swapnil.pawar@gullak.money>
2023-05-17 20:03:11 +0530

    running change - Home page lcoators-getters

---
+++ tests/pages/home.Page.js
@@ -0,0 +1,30 @@
+class HomePage{
+    // Getter methods
+    get signInButton(){
+        return $('=Sign in')
+    }
+
+    get signInWithEmailButton(){
+        return $('=Continue with email')
+    }
+
+    get signInWithGoogleButton(){
+        return $('#googleBtn')
+    }
+
+    get emailTextBox(){
+        return $('#regSignIn.email')
+    }
+
+    get passwordTextBox(){
+        return $('#regSignIn.password')
+    }
+
+    async enterEmailId(){
+        await emailTextBox().
+    }
+
+    async enterPassword(){
+        await 
+    }
+}
